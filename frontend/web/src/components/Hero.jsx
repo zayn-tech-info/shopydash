d6f59adc9139680c945a33e009126ba998b49d46
zayn-tech-info <zayntech.info@gmail.com>
2025-12-05 14:00:20 +0100

    feat: add initial Navbar and Hero components, including various image assets.

--- frontend/web/src/components/Hero.jsx
+++ frontend/web/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { MessageCircle } from "lucide-react";
-import HeroImg from "../assets/hero_sec.png";
+import HeroImg from "../assets/images/hero_sec";
 import { Button } from "./Button";
 
 export function Hero() {
